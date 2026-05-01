2b97dc4e3515b844fd7e3aa592d6aac72f42fc3c
marcelorim <marcelorim@gmail.com>
2026-05-01 19:00:49 -0300

    Ajuste na dinamica de carregamento da página.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const API = "https://script.google.com/macros/s/AKfycbxMATa0sIVTH4ZPJ2KApfw6pmQC_o9YN2lAkH4vmsP2z86tguWiu41qkil4cAKbKD_n/exec";
 
+/* ===============================
+   CHAMADA API GLOBAL
+================================= */
 async function api(action, params = {}) {
 
   let url = API + "?action=" + action;
@@ -9,60 +12,19 @@ async function api(action, params = {}) {
   });
 
   const resposta = await fetch(url);
-  const json = await resposta.json();
-
-  return json;
+  return await resposta.json();
 }
 
-function modoNovaLista(){
-    document.getElementById("menuInicial").style.display = "none";
-    document.getElementById("areaNova").style.display = "block";
-  }
-  
-  function cancelarNovaLista(){  
-    document.getElementById("areaNova").style.display = "none";
-    document.getElementById("menuInicial").style.display = "block";  
-  }
-  
-  async function criarLista(){  
-    const nome = document.getElementById("nomeLista").value.trim();  
-    if(!nome){
-      alert("Digite o nome da lista");
-      return;
-    }
-  
-    const r = await api("criarLista",{nomeLista:nome});  
-    idLista = r.data.idLista;  
-    finalizarLista(nome);  
-    renderItens([]);  
-  }
-  
-  async function selecionarLista(){  
-    idLista = document.getElementById("comboListas").value;  
-    if(!idLista) return;
-  
-    const nome =
-    document.getElementById("comboListas")
-    .options[
-      document.getElementById("comboListas").selectedIndex
-    ].text;
-  
-    finalizarLista(nome);  
-    const r = await api("buscarItensDaLista",{idLista:idLista});  
-    renderItens(r.data);
-  }
-  
-  function finalizarLista(nome){  
-    document.getElementById("menuInicial").style.display = "none";
-    document.getElementById("areaNova").style.display = "none";
-  
-    const div = document.getElementById("listaAtiva");  
-    div.style.display = "block";  
-    div.innerHTML = `
-      <div class="lista-box">
-        🛒 <strong>${nome.toUpperCase()}</strong>
-      </div>
-    `;
-      
-    document.getElementById("areaItens").style.display = "block";
-  }
+/* ===============================
+   ALERTA GLOBAL
+================================= */
+function aviso(msg){
+  alert(msg);
+}
+
+/* ===============================
+   CONFIRMAÇÃO GLOBAL
+================================= */
+function confirmar(msg){
+  return confirm(msg);
+}
