4ac798f5a7e8ab93601f1128e280916b7999e239
marcelorim <marcelorim@gmail.com>
2026-05-01 18:36:11 -0300

    Ajuste na dinamica de carregamento da página.

--- app.js
+++ app.js
@@ -15,11 +15,54 @@ async function api(action, params = {}) {
 }
 
 function modoNovaLista(){
-    document.getElementById("menuInicial").style.display="none";
-    document.getElementById("areaNova").style.display="block";
-}
-   
-function modoCarregar(){
-    document.getElementById("menuInicial").style.display="none";
-    document.getElementById("areaCarregar").style.display="block";
-}
+    document.getElementById("menuInicial").style.display = "none";
+    document.getElementById("areaNova").style.display = "block";
+  }
+  
+  function cancelarNovaLista(){  
+    document.getElementById("areaNova").style.display = "none";
+    document.getElementById("menuInicial").style.display = "block";  
+  }
+  
+  async function criarLista(){  
+    const nome = document.getElementById("nomeLista").value.trim();  
+    if(!nome){
+      alert("Digite o nome da lista");
+      return;
+    }
+  
+    const r = await api("criarLista",{nomeLista:nome});  
+    idLista = r.data.idLista;  
+    finalizarLista(nome);  
+    renderItens([]);  
+  }
+  
+  async function selecionarLista(){  
+    idLista = document.getElementById("comboListas").value;  
+    if(!idLista) return;
+  
+    const nome =
+    document.getElementById("comboListas")
+    .options[
+      document.getElementById("comboListas").selectedIndex
+    ].text;
+  
+    finalizarLista(nome);  
+    const r = await api("buscarItensDaLista",{idLista:idLista});  
+    renderItens(r.data);
+  }
+  
+  function finalizarLista(nome){  
+    document.getElementById("menuInicial").style.display = "none";
+    document.getElementById("areaNova").style.display = "none";
+  
+    const div = document.getElementById("listaAtiva");  
+    div.style.display = "block";  
+    div.innerHTML = `
+      <div class="lista-box">
+        🛒 <strong>${nome.toUpperCase()}</strong>
+      </div>
+    `;
+      
+    document.getElementById("areaItens").style.display = "block";
+  }
