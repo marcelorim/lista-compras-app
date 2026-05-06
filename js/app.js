37730bd7ff0efb7e2e94d900ec5704f19011691b
marcelorim <marcelorim@gmail.com>
2026-05-06 17:07:50 -0300

    Inclusão de rotinas para travar o zoom nas telas.

--- js/app.js
+++ js/app.js
@@ -114,3 +114,24 @@ async function executarComLoading(fn) {
     hideLoading();
   }
 }
+
+/* ===============================
+   BLOQUEIO DE ZOOM
+================================= */
+// 🔥 BLOQUEAR ZOOM (PINCH)
+document.addEventListener('touchstart', function (e) {
+  if (e.touches.length > 1) {
+      e.preventDefault();
+  }
+}, { passive: false });
+
+// 🔥 BLOQUEAR DOUBLE TAP ZOOM
+let lastTouchEnd = 0;
+
+document.addEventListener('touchend', function (e) {
+  const now = new Date().getTime();
+  if (now - lastTouchEnd <= 300) {
+      e.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
